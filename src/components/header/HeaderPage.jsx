3c7677c300a47a2fb4fc9e99fb9819d6ecfaf916
ishivamshukla <ishivamshukl@gmail.com>
2023-04-28 13:46:33 +0530

    add logo

--- src/components/header/HeaderPage.jsx
+++ src/components/header/HeaderPage.jsx
@@ -9,8 +9,7 @@ const HeaderPage = () => {
       <Container className=' justify-content-center mb-4 pb-4'>
         <Navbar.Brand  href="/">
           <img
-            // src={"https://cdn1.acedms.com/photos/images/droomresponsive/img/content/homepage-v5/droom-logo.png"}
-            src=''
+            src={"https://cdn1.acedms.com/photos/images/droomresponsive/img/content/homepage-v5/droom-logo.png"}
             width="240"
             height="30"
             className="d-inline-block align-top"

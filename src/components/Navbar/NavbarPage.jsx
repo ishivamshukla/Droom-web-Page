81d07d26b6eba524b7b6295ef1593fc4e63a579c
ishivamshukla <ishivamshukl@gmail.com>
2023-04-30 15:01:38 +0530

    update navbar

--- src/components/Navbar/NavbarPage.jsx
+++ src/components/Navbar/NavbarPage.jsx
@@ -6,41 +6,45 @@ import { FaHome, FaUser, FaCog, FaBell, FaEnvelope, FaChartBar } from 'react-ico
 
 const NavbarPage = () => {
   return (
-    <Container fluid  style={{width: '95% ', position: 'absolute', top: 120,  left: 0, right: 0 }} className={' bg-light start-50 translate-middle '}>
-    <Navbar fluid className={'navbar mx-auto'}  >
-      <Nav className={'nav mx-auto'}>
-        <Row className={'nav-row'}>
-        <Col className={'nav-col px-4'} >
+    <Container fluid  style={{width: '95%',  position: 'absolute', top: 120,  left: 0, right: 0 }} className={' bg-light start-50 translate-middle '}>
+      <Navbar bg={"light"} expand={"md"} >
+      <Container>
+        <Navbar.Toggle aria-controls={"basic-navbar-nav"}  />
+        <Navbar.Collapse id={"basic-navbar-nav"} className={'d-md-block d-lg-block justify-content-center'}>
+        <Nav className={'d-flex justify-content-center'}>
+        
+        <Row className={'d-flex flex-row'}>
+        <Col  className={'px-4'} >
           <Nav.Link href="/" className={"d-flex flex-column align-items-center"}>
             <FaHome size={34} color={'green'}/>
             <span style={{ fontSize: '12px' }}>Buy</span>
           </Nav.Link>
         </Col>
-        <Col className={'nav-col px-4'} >
+        <Col  className={'px-4'} >
         <Nav.Link href="/" className={"d-flex flex-column align-items-center"}>
             <FaUser size={34} color={'green'}/>
             <span style={{ fontSize: '12px' }}>Sell</span>
           </Nav.Link>
         </Col>
-        <Col className={'nav-col px-4'} >
+        <Col  className={'px-4'} >
         <Nav.Link href="/" className={"d-flex flex-column align-items-center"}>
             <FaCog size={34} color={'green'}/>
             <span style={{ fontSize: '12px' }}>Exchange</span>
           </Nav.Link>
         </Col>
-        <Col className={'nav-col px-4'} >
+        <Col  className={'px-4'} >
         <Nav.Link href="/" className={"d-flex flex-column align-items-center"}>
             <FaBell size={34} color={'green'} />
             <span style={{ fontSize: '12px' }}>Loan</span>
           </Nav.Link>
         </Col>
-        <Col className={'nav-col px-4'}  >
+        <Col  className={'px-4'}  >
         <Nav.Link href="/" className={"d-flex flex-column align-items-center"}>
             <FaEnvelope size={34} color={'green'} />
             <span style={{ fontSize: '12px' }}>Insurance</span>
           </Nav.Link>
         </Col>
-        <Col className={'nav-col px-4'} >
+        <Col  className={'px-4'} >
         <Nav.Link href="/" className={"d-flex flex-column align-items-center"}>
             <FaChartBar size={34} color={'green'}/>
             <span style={{ fontSize: '12px' }}>RSA</span>
@@ -48,6 +52,8 @@ const NavbarPage = () => {
         </Col>
         </Row>
       </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
     </Container>
   )
--- src/components/header/HeaderPage.jsx
+++ src/components/header/HeaderPage.jsx
@@ -5,8 +5,8 @@ import Navbar from 'react-bootstrap/Navbar';
 const HeaderPage = () => {
   return (
     <>
-    <Navbar className={'bg-body mb-4 pb-4'} >
-      <Container className={' justify-content-center mb-4 pb-4'}>
+    <Navbar  className={'bg-body mb-4 pb-4'} >
+      <Container  className={' justify-content-center mb-4 pb-4'}>
         <Navbar.Brand  href="/">
           <img
             src={"https://cdn1.acedms.com/photos/images/droomresponsive/img/content/homepage-v5/droom-logo.png"}
